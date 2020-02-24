5231f9010edbdcb15a4436c0555d4cf7534aa3d0
thomas <thomas.samy@epsi.fr>
2020-02-24 15:37:37 +0100

    ajout details activity

---
+++ gostyle/app/src/main/java/fr/epsi/gostyle/CodeDetailsActivity.java
@@ -0,0 +1,14 @@
+package fr.epsi.gostyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CodeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_details);
+    }
+}
